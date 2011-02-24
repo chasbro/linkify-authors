7fc612c6d878e74382e6a333d63488f818a4d4b1
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-24 22:08:32 +0000

    * Update to v2.0 * Add Linkify Authors widget * Rename `linkify_authors()` to `c2c_linkify_authors()` (but maintain a deprecated version for backwards compatibility) * Rename 'linkify_authors' action to 'c2c_linkify_authors' (but maintain support for backwards compatibility) * Add Template Tag, Screenshots, and Frequently Asked Questions sections to readme.txt * Add screenshot of widget admin * Changed Description in readme.txt * Note compatibility through WP 3.1+ * Update copyright date (2011)
    git-svn-id: https://plugins.svn.wordpress.org/linkify-authors/tags/2.0@350647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- linkify-authors.php
+++ linkify-authors.php
@@ -2,76 +2,30 @@
 /**
  * @package Linkify_Authors
  * @author Scott Reilly
- * @version 1.1
+ * @version 2.0
  */
 /*
 Plugin Name: Linkify Authors
-Version: 1.1
-Plugin URI: http://coffee2code.com/wp-plugins/linkify-authors
+Version: 2.0
+Plugin URI: http://coffee2code.com/wp-plugins/linkify-authors/
 Author: Scott Reilly
 Author URI: http://coffee2code.com
 Description: Turn a string, list, or array of author IDs and/or slugs into a list of links to those authors.
 
-Compatible with WordPress 2.8+, 2.9+.
+Compatible with WordPress 2.8+, 2.9+, 3.0+, 3.1+.
 
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/linkify-authors.zip and unzip it into your 
-/wp-content/plugins/ directory (or install via the built-in WordPress plugin installer).
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. Use the linkify_authors() template tag in one of your templates (be sure to pass it at least the first argument
-	indicating what author IDs and/or slugs to linkify -- the argument can be an array, a space-separate list, or a
-	comma-separated list).  Other optional arguments are available to customize the output.
-
-
-Examples:
-
-These are all valid calls:
-	<?php linkify_authors(3); ?>
-	<?php linkify_authors("3"); ?>
-	<?php linkify_authors("scott"); ?>
-	<?php linkify_authors("3 9 10"); ?>
-	<?php linkify_authors("scott bill alice"); ?>
-	<?php linkify_authors("scott 9 alice"); ?>
-	<?php linkify_authors("3,9,10"); ?>
-	<?php linkify_authors("scott,bill,alice"); ?>
-	<?php linkify_authors("scott,92,alice"); ?>
-	<?php linkify_authors("3, 9, 10"); ?>
-	<?php linkify_authors("scott, bill, alice"); ?>
-	<?php linkify_authors("scott, 92, alice"); ?>
-	<?php linkify_authors(array(43,92,102)); ?>
-	<?php linkify_authors(array("43","92","102")); ?>
-	<?php linkify_authors(array("scott","bill","alice")); ?>
-	<?php linkify_authors(array("scott",92,"alice")); ?>
-
-<?php linkify_authors("3 9"); ?>
-Displays something like:
-	<a href="http://yourblog.com/archives/author/admin">Scott</a>, 
-	<a href="http://yourblog.com/archives/author/billm">Bill</a>
-
-<ul><?php linkify_authors("3, 9", "<li>", "</li>", "</li><li>"); ?></ul>
-Displays something like:
-	<ul><li><a href="http://yourblog.com/archives/author/admin">Scott</a></li>
-	<li><a href="http://yourblog.com/archives/author/billm">Bill</a></li></ul>
-
-<?php linkify_authors(""); // Assume you passed an empty string as the first value ?>
-Displays nothing.
-
-<?php linkify_authors("", "", "", "", "", "No related authors."); // Assume you passed an empty string as the first value ?>
-Displays:
-	No related authors.
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/extend/plugins/linkify-authors/
 
 */
 
 /*
-Copyright (c) 2009-2010 by Scott Reilly (aka coffee2code)
+Copyright (c) 2009-2011 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -82,43 +36,53 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRA
 IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+require_once( dirname( __FILE__ ) . '/linkify-authors.widget.php' );
+
+if ( !function_exists( 'c2c_linkify_authors' ) ) :
 /**
  * Displays links to each of any number of authors specified via author IDs/slugs
- * 
+ *
+ * @since 2.0
+ *
  * @param int|string|array $authors A single author ID/slug, or multiple author IDs/slugs defined via an array, or multiple author IDs/slugs defined via a comma-separated and/or space-separated string
  * @param string $before (optional) Text to appear before the entire author listing (if authors exist or if 'none' setting is specified)
  * @param string $after (optional) Text to appear after the entire author listing (if authors exist or if 'none' setting is specified)
  * @param string $between (optional) Text to appear between all authors
- * @param string $before_last (optional) Text to appear between the second-to-last and last element, if not specified, 'between' value is used	
+ * @param string $before_last (optional) Text to appear between the second-to-last and last element, if not specified, 'between' value is used
  * @param string $none (optional) Text to appear when no authors have been found.  If blank, then the entire function doesn't display anything
  * @return none (Text is echoed; nothing is returned)
  */
-function linkify_authors( $authors, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
-	if ( empty($authors) )
+function c2c_linkify_authors( $authors, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
+	if ( empty( $authors ) )
 		$authors = array();
-	elseif ( !is_array($authors) )
-		$authors = explode(',', str_replace(array(', ', ' ', ','), ',', $authors));
+	elseif ( !is_array( $authors ) )
+		$authors = explode( ',', str_replace( array( ', ', ' ', ',' ), ',', $authors ) );
 
-	if ( empty($authors) ) $response = '';
-	else {
+	if ( empty( $authors ) ) {
+		$response = '';
+	} else {
 		$links = array();
 		foreach ( $authors as $id ) {
-			if ( 0 == (int)$id ) {
-				$author = get_userdatabylogin($id);
-				$id = $author->ID;
+			if ( 0 == (int) $id ) {
+				$author = get_userdatabylogin( $id );
+				if ( $author )
+					$id = $author->ID;
 			}
-			$title = get_author_name($id);
+			if ( !$id )
+				continue;
+			$title = get_the_author_meta( 'display_name', $id );
 			if ( $title )
 				$links[] = sprintf(
 					'<a href="%1$s" title="%2$s">%3$s</a>',
-					get_author_posts_url($id),
-					sprintf(__('Posts by %s'), esc_attr($title)),
+					get_author_posts_url( $id ),
+					esc_attr( sprintf( __( 'Posts by %s' ), $title ) ),
 					$title
 				);
 		}
-		if ( empty($before_last) ) $response = implode($links, $between);
-		else {
-			switch ( $size = sizeof($links) ) {
+		if ( empty( $before_last ) ) {
+			$response = implode( $between, $links );
+		} else {
+			switch ( $size = sizeof( $links ) ) {
 				case 1:
 					$response = $links[0];
 					break;
@@ -126,14 +90,31 @@ function linkify_authors( $authors, $before = '', $after = '', $between = ', ', 
 					$response = $links[0] . $before_last . $links[1];
 					break;
 				default:
-					$response = implode(array_slice($links,0,$size-1), $between) . $before_last . $links[$size-1];
+					$response = implode( $between, array_slice( $links, 0, $size-1 ) ) . $before_last . $links[$size-1];
 			}
 		}
 	}
-	if ( empty($response) ) {
-		if ( empty($none) ) return;
+	if ( empty( $response ) ) {
+		if ( empty( $none ) )
+			return;
 		$response = $none;
 	}
 	echo $before . $response . $after;
 }
+add_action( 'c2c_linkify_authors', 'c2c_linkify_authors', 10, 6 );
+endif;
+
+if ( !function_exists( 'linkify_authors' ) ) :
+/**
+ * Displays links to each of any number of authors specified via author IDs/slugs
+ *
+ * @since 1.0
+ * @deprecated 2.0 Use c2c_linkify_authors() instead
+ */
+function linkify_authors( $authors, $before = '', $after = '', $between = ', ', $before_last = '', $none = '' ) {
+	_deprecated_function( 'linkify_authors', '2.0', 'c2c_linkify_authors' );
+	return c2c_linkify_authors( $authors, $before, $after, $between, $before_last, $none );
+}
+add_action( 'linkify_authors', 'linkify_authors', 10, 6 ); // Deprecated
+endif;
 ?>
